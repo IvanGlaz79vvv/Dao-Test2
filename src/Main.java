bbb2f076667bd0de5150e98904865a1ac52c5164
IvanGlaz79vvv <ivangl79@gmail.com>
2022-10-23 13:13:30 +0300

    Новый Dao

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Hello world!");
+
+//        UserService.createTable(); //Создать таблицу
+
+//        UserService.saveUser(); //Добавить юзера
+
+//        UserService.getAllUsers(); //Показать всех юзеров
+
+//        UserService.getUserById(); //Выбрать юзера по id
+
+//        UserService.getUserByName(); //Выбрать юзера по name
+
+//        UserService.getUserByPosition(); //Выбрать юзера по position
+
+//        UserService.getUserByDate(); //Выбрать юзера по date
+
+//        UserService.removeUserById(); //Удалить юзера по id
+
+// System.out.println("\nid: " + arrayUsersName.get(i).getId() + "\nname: " + arrayUsersName.get(i).getName() + "\nposition: " + arrayUsersName.get(i).getPosition() + "\ndate: " + arrayUsersName.get(i).getDate());
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        System.exit(0);
+    }
+}
